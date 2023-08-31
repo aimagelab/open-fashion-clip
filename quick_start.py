9fe493af9ec67c1daa837c51d738f5fb6b6df5fb
beppefolder <giuseppe.cartella@unimore.it>
2023-08-31 13:45:14 +0200

    Code and checkpoint release

--- quick_start.py
+++ quick_start.py
@@ -6,7 +6,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Load the model
 clip_model, _, preprocess = open_clip.create_model_and_transforms('ViT-B/32')
-state_dict = torch.load('weights/openfashionclip.pt', map_location=device)
+state_dict = torch.load(<your_checkpoint_path>, map_location=device)
 clip_model.load_state_dict(state_dict['CLIP'])
 clip_model = clip_model.eval().requires_grad_(False).to(device)
 tokenizer = open_clip.get_tokenizer('ViT-B-32')
